565b8d81339457cb6b41915cf2d4f05a266b9aa5
JenniferTh <J-Theloy@gmx.de>
2015-05-04 11:25:37 +0200

    Kugel eingefügt

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -32,6 +32,46 @@ Vec3 punktEe(2, 2, 0);
 Vec3 punktGg(0, 2, 2);
 Vec3 punktHh(2, 2, 2);
 
+// draw a sphere composed of triangles
+void DrawSphere(const Vec3& ctr, double r){
+  int     i, j,
+          n1 = 6, n2 = 12;
+  Vec3    normal, v1;
+  double  a1, a1d = M_PI / n1,
+          a2, a2d = M_PI / n2,
+          s1, s2,
+          c1, c2;
+
+  glShadeModel(GL_SMOOTH);
+  for(i = 0; i < n1; i++){
+    a1 = i * a1d;
+
+    glBegin(GL_TRIANGLE_STRIP);
+    for(j = 0; j <= n2; j++){
+      a2 = (j + .5 * (i % 2)) * 2 * a2d;
+
+      s1 = sin(a1);
+      c1 = cos(a1);
+      s2 = sin(a2);
+      c2 = cos(a2);
+      normal = c1 * XVec3 + s1 * (c2 * YVec3 + s2 * ZVec3);
+      v1 = ctr + r * normal;
+      glNormal3dv(normal.p);
+      glVertex3dv(v1.p);
+
+      s1 = sin(a1 + a1d);
+      c1 = cos(a1 + a1d);
+      s2 = sin(a2 + a2d);
+      c2 = cos(a2 + a2d);
+      normal = c1 * XVec3 + s1 * (c2 * YVec3 + s2 * ZVec3);
+      v1 = ctr + r * normal;
+      glNormal3dv(normal.p);
+      glVertex3dv(v1.p);
+    }
+    glEnd();
+  }
+}
+
 void SetMaterialColor(int side, double r, double g, double b) {
   float	amb[4], dif[4], spe[4];
   int mat;
@@ -202,6 +242,8 @@ void Preview() {
   //  glRotated(alpha_, 0, 2, 1);
   //  alpha_ += 2;
   drawCube(2);
+  SetMaterialColor(3, 1, 0, 0);
+  DrawSphere(Vec3( 5, 0, 0), .5);
 
   //TestSection
   /*punktEe.Print("a");
